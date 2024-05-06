faa4fcd5e0f73b413dc1b28f5b0f739855e1e29f
吴仙杰 <wuxianjiezh@gmail.com>
2024-05-06 12:56:45 +0800

    启动 Eureka Naming Server

---
+++ naming-server/src/main/java/net/wuxianjie/currencyconversionservice/auth/TokenAuthImpl.java
@@ -0,0 +1,16 @@
+package net.wuxianjie.currencyconversionservice.auth;
+
+import org.springframework.stereotype.Service;
+
+import net.wuxianjie.webkit.security.CurrentUserInfo;
+import net.wuxianjie.webkit.security.TokenAuth;
+
+@Service
+public class TokenAuthImpl implements TokenAuth {
+
+    @Override
+    public CurrentUserInfo authenticate(String accessToken) {
+        return null;
+    }
+
+}
