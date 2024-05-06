2ddd56d2c00954170f633166a7843efb879d5b41
吴仙杰 <wuxianjiezh@gmail.com>
2024-05-06 21:17:46 +0800

    Spring Cloud Gateway 全局路径

---
+++ api-gateway/src/main/java/net/wuxianjie/apigateway/config/LoggingFilter.java
@@ -0,0 +1,22 @@
+package net.wuxianjie.apigateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("请求路径: {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+
+}
