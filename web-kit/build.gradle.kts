83e1e45de3360617849f311c98920624c2e12aee
吴仙杰 <wuxianjiezh@gmail.com>
2024-05-07 22:20:42 +0800

    配置 Spring Boot Actuator
    (cherry picked from commit 093593b0b7adf230d75132880557d6e0a2ed95d4)

--- web-kit/build.gradle.kts
+++ web-kit/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-web")
     api("org.springframework.boot:spring-boot-starter-security")
     api("org.springframework.boot:spring-boot-starter-validation")
+    api("org.springframework.boot:spring-boot-starter-actuator")
     api("org.springframework.boot:spring-boot-starter-aop")
     api("org.springframework.boot:spring-boot-configuration-processor")
 }
--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,6 +1,26 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 plugins {
     id("org.springframework.boot") version "3.2.5"
     id("org.graalvm.buildtools.native") version "0.9.28" // `./gradlew nativeCompile` - 编译本地可执行文件
+}
+
+version = "v1.0.0"
+
+springBoot {
+    buildInfo {
+        properties {
+            excludes.add("time") // 移除默认时间戳（ISO 8601 格式 `yyyy-MM-dd'T'HH:mm:ssZ`）
+            additional.set(
+                mapOf(
+                    "timestamp" to LocalDateTime.now()
+                        .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+                )
+            )
+            name = "Web 项目"
+        }
+    }
 }
 
 dependencies {
