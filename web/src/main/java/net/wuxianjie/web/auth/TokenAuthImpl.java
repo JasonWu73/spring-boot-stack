e38945be90dd68322dd7eb86bf4d4cb83399c9f9
吴仙杰 <wuxianjiezh@gmail.com>
2024-05-13 21:00:27 +0800

    优化 Web MVC 全局异常处理，增加全局限流异常处理

--- web/src/main/java/net/wuxianjie/web/auth/TokenAuthImpl.java
+++ web/src/main/java/net/wuxianjie/web/auth/TokenAuthImpl.java
@@ -6,15 +6,19 @@ import org.springframework.stereotype.Service;
 
 import net.wuxianjie.myspringbootstarter.security.CurrentUser;
 import net.wuxianjie.myspringbootstarter.security.TokenAuth;
+import net.wuxianjie.myspringbootstarter.security.TokenAuthException;
 
 @Service
 public class TokenAuthImpl implements TokenAuth {
 
     @Override
-    public CurrentUser authenticate(String accessToken) {
-        return new CurrentUser(
-            1, "root", "超级管理员", List.of("root"),
-            "root-token", "root-refresh"
-        );
+    public CurrentUser authenticate(String accessToken) throws TokenAuthException {
+        if (accessToken.equals("root-token")) {
+            return new CurrentUser(
+                1, "root", "超级管理员", List.of("root"),
+                "root-token", "root-refresh"
+            );
+        }
+        throw new TokenAuthException("无效的令牌");
     }
 }
