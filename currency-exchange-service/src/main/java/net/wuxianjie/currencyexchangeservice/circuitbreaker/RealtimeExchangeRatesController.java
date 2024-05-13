c85da6e7c7e66099b444747de596deeedce3de51
吴仙杰 <wuxianjiezh@gmail.com>
2024-05-13 21:01:55 +0800

    全局限流测试

--- currency-exchange-service/src/main/java/net/wuxianjie/currencyexchangeservice/circuitbreaker/RealtimeExchangeRatesController.java
+++ currency-exchange-service/src/main/java/net/wuxianjie/currencyexchangeservice/circuitbreaker/RealtimeExchangeRatesController.java
@@ -1,11 +1,10 @@
 package net.wuxianjie.currencyexchangeservice.circuitbreaker;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,24 +22,23 @@ public class RealtimeExchangeRatesController {
         this.restClient = restClient;
     }
 
+    // `watch -n 0.1 curl http://127.0.0.1:8000/api/v1/realtime-exchange-rates`
     @GetMapping("/realtime-exchange-rates")
-    // @Retry(name = "realtimeExchangeRatesService", fallbackMethod = "realtimeExchangeRatesFallback")
-    @CircuitBreaker(name = "realtimeExchangeRatesService", fallbackMethod = "realtimeExchangeRatesFallback")
+    // @Retry(name = "#root.methodName", fallbackMethod = "realtimeExchangeRatesFallback")
+    // @CircuitBreaker(name = "#root.methodName", fallbackMethod = "realtimeExchangeRatesFallback")
+    @RateLimiter(name = "#root.methodName")
     public String getRealtimeExchangeRates() {
         LOG.info("调用实时汇率服务");
-        ResponseEntity<String> entity = restClient.get()
-            .uri("http://127.0.0.1:9090/dummy-dummy")
-            .retrieve()
-            .toEntity(String.class);
-        return entity.getBody();
+        // ResponseEntity<String> entity = restClient.get()
+        //     .uri("http://127.0.0.1:9090/dummy-dummy")
+        //     .retrieve()
+        //     .toEntity(String.class);
+        // return entity.getBody();
+        return "Ok";
     }
 
     @SuppressWarnings("unused")
     private String realtimeExchangeRatesFallback(Throwable throwable) {
-        // LOG.error(
-        //     "调用实时汇率服务失败 [{}: {}]",
-        //     throwable.getClass().getName(), throwable.getMessage()
-        // );
         return "最近汇率为 1 比 7.23";
     }
 }
