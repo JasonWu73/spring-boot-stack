de38fa3da619e19970f54736363d96ff0e600f9e
吴仙杰 <wuxianjiezh@gmail.com>
2024-05-15 20:49:44 +0800

    加入分布式链路追踪测试：Micrometer + OpenTelemeter + Zipkin

--- currency-exchange-service/build.gradle.kts
+++ currency-exchange-service/build.gradle.kts
@@ -5,9 +5,15 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation("com.mysql:mysql-connector-j")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter")
+
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("io.github.resilience4j:resilience4j-spring-boot3")
+
+    implementation("io.micrometer:micrometer-observation")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.opentelemetry:opentelemetry-exporter-zipkin")
 }
