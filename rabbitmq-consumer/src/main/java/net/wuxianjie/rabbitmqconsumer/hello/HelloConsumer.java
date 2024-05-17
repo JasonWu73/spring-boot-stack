bd7ec05bd6ba163593bcc32515003f8d4d03cce0
吴仙杰 <wuxianjiezh@gmail.com>
2024-05-17 13:29:21 +0800

    清理代码

--- rabbitmq-consumer/src/main/java/net/wuxianjie/rabbitmqconsumer/hello/HelloConsumer.java
+++ rabbitmq-consumer/src/main/java/net/wuxianjie/rabbitmqconsumer/hello/HelloConsumer.java
@@ -2,7 +2,6 @@ package net.wuxianjie.rabbitmqconsumer.hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
--- rabbitmq-producer/src/main/java/net/wuxianjie/rabbitmqproducer/hello/HelloProducer.java
+++ rabbitmq-producer/src/main/java/net/wuxianjie/rabbitmqproducer/hello/HelloProducer.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
