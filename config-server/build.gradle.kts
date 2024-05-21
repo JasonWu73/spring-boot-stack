ce6fdbdb9cea41bb66df36c34b715cebe8a9fa44
吴仙杰 <wuxianjiezh@gmail.com>
2024-05-21 19:06:14 +0800

    优化部署

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -3,5 +3,6 @@ plugins {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-config-server")
 }
